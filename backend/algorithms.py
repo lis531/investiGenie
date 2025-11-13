4879c1f4a81f8e4109ebb718adfd268175df8db9
Tymoteusz Dawid <128647929+terek07@users.noreply.github.com>
2025-11-13 09:35:35 +0100

    fix: change returns type to tuple

--- backend/algorithms.py
+++ backend/algorithms.py
@@ -5,22 +5,22 @@ def buy_after_3_consecutive_down_days(i:int, prices: list[float], start_cash: fl
         two_days_ago = prices[i - 2]
         yesterday = prices[i - 1]
         if three_days_ago > two_days_ago > yesterday > price:
-            return "buy"
-    return "none"
+            return "buy",
+    return "none",
 
 def buy_everyday(i:int, prices: list[float], start_cash: float, monthly_cash: float, cash:float):
-    return "buy"
+    return "buy",
 
 def buy_and_hold(i:int, prices: list[float], start_cash: float, monthly_cash: float, cash:float):
     if i==0:
-        return "buy"
-    return "none"
+        return "buy",
+    return "none",
 
 def buy_the_dip(i:int, prices:list[float], start_cash:float, monthly_cash:float, cash:float,  dip_threshold:float=0.05, lookback_window:int=10):
     if i < 1:
-        return "none", 0
+        return "none",
     recent_high = max(prices[max(0, i - lookback_window):i])
     price = prices[i]
     if price < recent_high * (1 - dip_threshold):
-        return "buy"
-    return "none"
+        return "buy",
+    return "none",
